08c0e7a85a3746fe644acfade3a7e7a9c6b59089
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-08-19 16:23:25 -0400

    done

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,9 +20,15 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT FROM"
+  "SELECT title FROM series
+  INNER JOIN characters ON series.author_id = characters.author_id
+  WHERE characters.species = 'human'
+  GROUP BY title LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT FROM"
+  "SELECT characters.name, COUNT(*) as book_count FROM character_books 
+  INNER JOIN characters ON character_books.character_id = characters.id 
+  GROUP BY characters.name 
+  ORDER BY book_count DESC, characters.name;"
 end
